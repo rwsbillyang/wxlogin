2727009ac611db8ec9df7b2d9928577c8dca54f7
BillYang <rwsbillyang@qq.com>
2022-12-04 11:33:43 +0800

    V0.9.17 fix oa account login redirect_uri spell mistake when force to need user info

--- src/WxOauthLoginPageOA.tsx
+++ src/WxOauthLoginPageOA.tsx
@@ -2,16 +2,15 @@ import React, { useEffect, useState } from 'react';
 
 import { Block, Page } from 'framework7-react';
 
-import {currentHost, StorageType} from "@rwsbillyang/usecache"
+import { currentHost, StorageType } from "@rwsbillyang/usecache";
 
-import { saveValue, WxAuthHelper } from './WxOauthHelper';
+import { WxLoginConfig } from './Config';
 import { LoginParam } from './datatype/LoginParam';
 import { NeedUserInfoType } from './datatype/NeedUserInfoType';
 import { SnsScope } from './datatype/SnsScope';
 import { randomAlphabetNumber } from './random';
 import { pageCenter } from './style';
-import { WxLoginConfig } from './Config';
-import { WxOaAccountAuthBean } from './datatype/AuthBean';
+import { saveValue } from './WxOauthHelper';
 
 
 
@@ -27,16 +26,21 @@ const NotifyPath = {
  * @param params LoginParam
  * @returns 腾讯授权链接url，将重定向到该url进入获取用户openId，或用户授权的腾讯页面
  */
-export const authorizeUrl = (params: LoginParam, openId?: string) => {
+export const authorizeUrl = (params: LoginParam) => {
     //只有强制获取时，则直接进入step2，否则交由后端继续判断
     const forceNeed = params.needUserInfo === NeedUserInfoType.ForceNeed
     const notifyPath = forceNeed ? NotifyPath.notify2 : NotifyPath.notify1
     const scope = forceNeed ? SnsScope.userInfo : SnsScope.base
 
-    //构建callback notify的url
-    let url = `${currentHost()}${notifyPath}/${params.appId}/${params.needUserInfo}`
-    if (openId) url += ("/" + openId)
-    if (params.owner) url += ("/" + params.owner)
+    //构建callback notify的url,后端notify url如下：
+    // $notifyPath1/{appId}/{needUserInfo}/{owner?}
+    //"$notifyPath2/{appId}"
+    let url = `${currentHost()}${notifyPath}/${params.appId}`
+    if(!forceNeed){//notify1
+        url += ("/" + params.needUserInfo)
+        if (params.owner) url += ("/" + params.owner)
+    }
+    
     const redirectUri = encodeURI(url)
     console.log("redirectUri=" + url + ", after encodeURI=" + redirectUri)
 
@@ -57,15 +61,14 @@ export const WxOauthLoginPageOA: React.FC<LoginParam> = (props: LoginParam) => {
         if (!appId) {
             setStatus("no appId, please set appId in query parameters")
         } else {
-            const bean = WxAuthHelper.getAuthBean(true) || WxAuthHelper.getAuthBean(false) 
-            const user = (bean)? bean as WxOaAccountAuthBean : undefined
-
-            const openId = user?.guest?.openId || user?.guest?.openId
+            //const bean = WxAuthHelper.getAuthBean(true) || WxAuthHelper.getAuthBean(false) 
+            //const user = (bean)? bean as WxOaAccountAuthBean : undefined
+            //const openId = user?.guest?.openId || user?.guest?.openId
 
             if (from) saveValue("from", from)
             saveValue("authStorageType", authStorageType?.toString() || StorageType.BothStorage.toString())
 
-            const url = authorizeUrl(props, openId)
+            const url = authorizeUrl(props)
             if(WxLoginConfig.JumpToAuthrize)
                 window.location.href = url
         }
